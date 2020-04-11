e86f2fbb59c264ef45d28f9c31b02dbbecdd7f24
marcus-bornman <marcus.bornman@frei.one>
2020-04-11 19:59:49 +0200

    added invite_only_docs package

---
+++ invite_only_docs/lib/src/id_document_info/id_document_info.g.dart
@@ -0,0 +1,36 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'id_document_info.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$IdBookInfo _$_$IdBookInfoFromJson(Map<String, dynamic> json) {
+  return _$IdBookInfo();
+}
+
+Map<String, dynamic> _$_$IdBookInfoToJson(_$IdBookInfo instance) =>
+    <String, dynamic>{};
+
+_$IdCardInfo _$_$IdCardInfoFromJson(Map<String, dynamic> json) {
+  return _$IdCardInfo();
+}
+
+Map<String, dynamic> _$_$IdCardInfoToJson(_$IdCardInfo instance) =>
+    <String, dynamic>{};
+
+_$DriversLicenseInfo _$_$DriversLicenseInfoFromJson(Map<String, dynamic> json) {
+  return _$DriversLicenseInfo();
+}
+
+Map<String, dynamic> _$_$DriversLicenseInfoToJson(
+        _$DriversLicenseInfo instance) =>
+    <String, dynamic>{};
+
+_$PassportInfo _$_$PassportInfoFromJson(Map<String, dynamic> json) {
+  return _$PassportInfo();
+}
+
+Map<String, dynamic> _$_$PassportInfoToJson(_$PassportInfo instance) =>
+    <String, dynamic>{};
