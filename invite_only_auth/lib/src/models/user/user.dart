76c959a01d3c6232a6e9c4d577ce75ca6f130b71
marcus-bornman <marcus.bornman@frei.one>
2020-04-11 21:19:42 +0200

    Remove identity documents from user.dart

--- invite_only_auth/lib/src/models/user/user.dart
+++ invite_only_auth/lib/src/models/user/user.dart
@@ -1,6 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
-import 'package:invite_only_users/invite_only_users.dart';
 
 part 'user.freezed.dart';
 
@@ -11,10 +10,6 @@ abstract class User with _$User {
   const factory User({
     @required String id,
     @required String phoneNumber,
-    @nullable IdBookInfo idBookInfo,
-    @nullable IdCardInfo idCardInfo,
-    @nullable DriversLicenseInfo driversLicenseInfo,
-    @nullable PassportInfo passportInfo,
   }) = _User;
 
   factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
