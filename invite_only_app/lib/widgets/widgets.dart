33df5bbd8767d7cf326208228c0de15c7f227d56
marcus-bornman <marcus.bornman@frei.one>
2020-04-13 21:22:59 +0200

    Added error_dialog.dart

--- invite_only_app/lib/widgets/widgets.dart
+++ invite_only_app/lib/widgets/widgets.dart
@@ -1,6 +1,8 @@
 export 'access_info_card.dart';
-export 'otp_dialog.dart';
+export 'error_dialog.dart';
+export 'loading_scaffold.dart';
 export 'phone_number_search_delegate.dart';
+export 'phone_verification_dialog.dart';
 export 'space_card.dart';
 export 'space_list.dart';
 export 'user_home_drawer.dart';
