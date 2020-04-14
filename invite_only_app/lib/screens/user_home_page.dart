c0f9d6619d55f5c0a95ad08fc4922fbd7fb24659
marcus-bornman <marcus.bornman@frei.one>
2020-04-14 13:29:38 +0200

    Remove responsive_builder from UserHomePage

--- invite_only_app/lib/screens/user_home_page.dart
+++ invite_only_app/lib/screens/user_home_page.dart
@@ -1,21 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:invite_only/widgets/widgets.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 class UserHomePage extends StatelessWidget {
   static const ROUTE = '/';
 
   @override
   Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-      watch: null,
-      mobile: _mobile(context),
-      tablet: _tablet(context),
-      desktop: _desktop(context),
-    );
-  }
-
-  Widget _mobile(BuildContext context) {
     return Scaffold(
       drawer: UserHomeDrawer(),
       appBar: AppBar(
@@ -23,13 +13,5 @@ class UserHomePage extends StatelessWidget {
       ),
       body: SpaceList(),
     );
-  }
-
-  Widget _tablet(BuildContext context) {
-    return null;
-  }
-
-  Widget _desktop(BuildContext context) {
-    return null;
   }
 }
