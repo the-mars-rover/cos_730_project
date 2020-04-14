0e2b49bcae18b4eecb3a50b4edb36e8128b6e292
marcus-bornman <marcus.bornman@frei.one>
2020-04-14 13:32:01 +0200

    Moved user_home_drawer.dart into

--- invite_only_app/lib/screens/user_home_page.dart
+++ invite_only_app/lib/screens/user_home_page.dart
@@ -1,13 +1,71 @@
 import 'package:flutter/material.dart';
 import 'package:invite_only/widgets/widgets.dart';
 
+import 'screens.dart';
+
 class UserHomePage extends StatelessWidget {
   static const ROUTE = '/';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: UserHomeDrawer(),
+      drawer: Drawer(
+        child: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(left: 24.0, right: 24.0),
+                child: Image.asset("assets/logo_v5.png"),
+              ),
+              Divider(),
+              ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  ListTile(
+                    leading: Icon(Icons.person),
+                    title: Text("My Profile"),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => UserProfilePage(),
+                        ),
+                      );
+                    },
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.add_location),
+                    title: Text("Create Space"),
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => CreateSpacePage(),
+                      ));
+                    },
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text("About Us"),
+                  ),
+                  Divider(),
+                  ListTile(
+                    leading: Icon(Icons.exit_to_app),
+                    title: Text("Sign Out"),
+                    onTap: () {
+                      Navigator.of(context).pushAndRemoveUntil(
+                        MaterialPageRoute(
+                          builder: (context) => AuthenticatePage(),
+                        ),
+                            (route) => false,
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         title: Text("Invite Only"),
       ),
