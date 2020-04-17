97a234a73586a4ff6c6f61e2c0ae45e1d4e8bcb3
marcus-bornman <marcus.bornman@frei.one>
2020-04-17 14:29:06 +0200

    Added docs to documented_user.dart

--- invite_only_docs/lib/src/models/documented_user/documented_user.dart
+++ invite_only_docs/lib/src/models/documented_user/documented_user.dart
@@ -1,5 +1,5 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:flutter/foundation.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:invite_only_docs/src/models/id_document/id_document.dart';
 
 part 'documented_user.freezed.dart';
@@ -9,10 +9,19 @@ part 'documented_user.g.dart';
 @freezed
 abstract class DocumentedUser with _$DocumentedUser {
   const factory DocumentedUser({
+    /// The identifier for the user - can be anything as long as it is unique.
     @required String id,
+
+    /// The Id book stored for this user, null if no ID book is stored.
     IdBook idBook,
+
+    /// The Id card stored for this user, null if no ID card is stored.
     IdCard idCard,
+
+    /// The Driver's license stored for this user, null if no Driver's license is stored.
     DriversLicense driversLicense,
+
+    /// The Passport stored for this user, null if no Passport is stored.
     Passport passport,
   }) = _DocumentedUser;
 
