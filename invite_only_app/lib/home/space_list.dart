964f1019a0c14c9dec078d8f4ab055ca6ebd166c
marcus-bornman <marcus.bornman@frei.one>
2020-04-18 13:29:26 +0200

    Removed responsive_builder from space_list.dart

--- invite_only_app/lib/home/space_list.dart
+++ invite_only_app/lib/home/space_list.dart
@@ -1,19 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:invite_only/home/home.dart';
-import 'package:responsive_builder/responsive_builder.dart';
 
 class SpaceList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ScreenTypeLayout(
-      watch: null,
-      mobile: _mobile(context),
-      tablet: _tablet(context),
-      desktop: _desktop(context),
-    );
-  }
-
-  Widget _mobile(BuildContext context) {
     return Scaffold(
       body: ListView(
         padding: EdgeInsets.all(8.0),
@@ -22,21 +12,5 @@ class SpaceList extends StatelessWidget {
         ],
       ),
     );
-  }
-
-  Widget _tablet(BuildContext context) {
-    return Scaffold(
-      body: GridView.count(
-        padding: EdgeInsets.all(8.0),
-        crossAxisCount: 3,
-        children: <Widget>[
-          SpaceCard(),
-        ],
-      ),
-    );
-  }
-
-  Widget _desktop(BuildContext context) {
-    return null;
   }
 }
