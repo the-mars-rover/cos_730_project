64c7fc52e1030a0e5bc05ee5eacf4aad27615ec4
marcus-bornman <marcus.bornman@frei.one>
2020-04-18 07:52:32 +0200

    Some additional documentation

--- invite_only_spaces/lib/src/models/controlled_space/controlled_space.dart
+++ invite_only_spaces/lib/src/models/controlled_space/controlled_space.dart
@@ -4,6 +4,7 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'controlled_space.freezed.dart';
 part 'controlled_space.g.dart';
 
+/// A [ControlledSpace] is any space to which access is being controlled.
 @freezed
 abstract class ControlledSpace with _$ControlledSpace {
   const factory ControlledSpace({
@@ -26,12 +27,23 @@ abstract class ControlledSpace with _$ControlledSpace {
     int maxCapacity,
 
     /// A list of phone numbers identifying managers of this space
+    ///
+    /// Managers have the authority to:
+    /// * Edit the properties of the [ControlledSpace]
+    /// * Grant/Deny an [Access] to the [ControlledSpace]
+    /// * Generate [Invite]'s for the [ControlledSpace]
     List<String> managerPhones,
 
     /// A list of phone numbers identifying guards of this space
+    ///
+    /// Guards have the authority to:
+    /// * Grant/Deny an [Access] to the [ControlledSpace]
     List<String> guardPhones,
 
     /// A list of phone numbers identifying inviters of this space
+    ///
+    /// Inviters have the authority to:
+    /// * Generate [Invite]'s for the [ControlledSpace]
     List<String> inviterPhones,
   }) = _ControlledSpace;
 
--- invite_only_spaces/lib/src/models/models.dart
+++ invite_only_spaces/lib/src/models/models.dart
@@ -1,2 +1,4 @@
+export 'access/access.dart';
 export 'controlled_space/controlled_space.dart';
+export 'id_document/id_document.dart';
 export 'invite/invite.dart';
