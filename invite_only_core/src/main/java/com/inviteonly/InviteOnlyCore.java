b95e5c1650595f8e0a28694774031fa4b70f53e7
marcus-bornman <marcus.bornman@frei.one>
2020-06-07 21:44:55 +0200

    Removed unnecessary code in main method

--- invite_only_core/src/main/java/com/inviteonly/InviteOnlyCore.java
+++ invite_only_core/src/main/java/com/inviteonly/InviteOnlyCore.java
@@ -1,12 +1,7 @@
 package com.inviteonly;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
 
 /**
  * This class serves as an entry point for the Spring Boot app.
@@ -15,16 +10,5 @@ import java.io.IOException;
 public class InviteOnlyCore {
 	public static void main(final String[] args) {
 		SpringApplication.run(InviteOnlyCore.class, args);
-
-		try {
-			FirebaseOptions options = new FirebaseOptions.Builder()
-					.setCredentials(GoogleCredentials.getApplicationDefault())
-					.build();
-			if(FirebaseApp.getApps().isEmpty()) {
-				FirebaseApp.initializeApp(options);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
 }
