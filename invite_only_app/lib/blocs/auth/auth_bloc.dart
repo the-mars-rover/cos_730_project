09732eaf6b1be35e26a7f0e60cb963800bc2b5de
marcus-bornman <marcus.bornman@frei.one>
2020-06-07 13:03:09 +0200

    Merged phone_verification functionality into uth_bloc and auth_page

--- invite_only_app/lib/blocs/auth/auth_bloc.dart
+++ invite_only_app/lib/blocs/auth/auth_bloc.dart
@@ -22,6 +22,22 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       yield* _mapInitializeAuthToState(event);
     }
 
+    if (event is SendSmsCode) {
+      yield* _mapSendSmsCodeToState(event);
+    }
+
+    if (event is NotifyCodeSent) {
+      yield* _mapNotifyCodeSentToState(event);
+    }
+
+    if (event is SubmitSmsCode) {
+      yield* _mapSubmitSmsCodeToState(event);
+    }
+
+    if (event is FailVerification) {
+      yield* _mapFailVerificationToState(event);
+    }
+
     if (event is SignIn) {
       yield* _mapSignInToState(event);
     }
@@ -42,6 +58,29 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
     }
   }
 
+  Stream<AuthState> _mapSendSmsCodeToState(SendSmsCode event) async* {
+    yield AuthInProgress();
+
+    try {
+      await _inviteOnlyRepo.verifyPhoneNumber(
+        phoneNumber: event.phoneNumber,
+        retrievalTimeout: Duration(seconds: 60),
+        verificationCompleted: (authCredential) {
+          this.add(SignIn(authCredential));
+        },
+        verificationFailed: (authException) {
+          this.add(FailVerification(authException));
+        },
+        codeSent: (verificationId, [forceResendingToken]) {
+          this.add(NotifyCodeSent(event.phoneNumber, verificationId));
+        },
+      );
+    } catch (e) {
+      yield AuthFailed(
+          'Sorry, an unexpected error occurred. Please try again later.');
+    }
+  }
+
   Stream<AuthState> _mapSignInToState(SignIn event) async* {
     yield AuthInProgress();
 
@@ -50,8 +89,35 @@ class AuthBloc extends Bloc<AuthEvent, AuthState> {
       String phoneNumber = await _inviteOnlyRepo.currentUser();
 
       yield UserAuthenticated(phoneNumber);
-    } on AuthFailure catch (e) {
-      yield AuthFailed('Sign in failed: ${e.reason}');
+    } on AuthFailure {
+      yield AuthFailed(
+          'The phone number could not be verified. Please try again.');
+    }
+  }
+
+  Stream<AuthState> _mapFailVerificationToState(FailVerification event) async* {
+    yield AuthFailed(
+        'The phone number could not be verified. Please try again.');
+  }
+
+  Stream<AuthState> _mapNotifyCodeSentToState(NotifyCodeSent event) async* {
+    yield SmsCodeSent(event.phoneNumber, event.verificationId);
+  }
+
+  Stream<AuthState> _mapSubmitSmsCodeToState(SubmitSmsCode event) async* {
+    var currentState = state;
+    if (currentState is SmsCodeSent) {
+      try {
+        var authCredential = _inviteOnlyRepo.getAuthCredential(
+          currentState.verificationId,
+          event.smsCode,
+        );
+
+        this.add(SignIn(authCredential));
+      } catch (e) {
+        yield AuthFailed(
+            'The phone number could not be verified. Please try again.');
+      }
     }
   }
 
---
+++ invite_only_app/lib/widgets/other/error_message.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class ErrorMessage extends StatelessWidget {
+  final String message;
+  final Function onRetry;
+
+  const ErrorMessage(this.message, {Key key, @required this.onRetry})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        width: 250.0,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(Icons.error_outline, color: Colors.amber, size: 64.0),
+            Container(height: 24.0),
+            Text(
+              'Oops, an error occurred!',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Container(height: 16.0),
+            Text(message, textAlign: TextAlign.center),
+            FlatButton(
+              child: Text(
+                'Try Again',
+                style: TextStyle(color: Theme.of(context).primaryColor),
+              ),
+              onPressed: onRetry,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
