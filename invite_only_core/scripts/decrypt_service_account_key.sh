08fbebec342f11a1972c3953b6aeb46f307300c4
marcus-bornman <marcus.bornman@frei.one>
2020-06-07 22:43:33 +0200

    Attempted fix of workflow failing

--- invite_only_core/scripts/decrypt_service_account_key.sh
+++ invite_only_core/scripts/decrypt_service_account_key.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 # This script is used to decrypt the service-account-key for the Google cloud platform service account.
-mkdir $HOME/secrets
 gpg --quiet --batch --yes --decrypt --passphrase="$GCP_CREDENTIALS_PASSWORD" \
---output service-account-key.json service-account-key.json.gpg
+--output /service-account-key.json scripts/service-account-key.json.gpg
