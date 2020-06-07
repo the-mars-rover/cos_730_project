f476e0ff041339f19f4cfcf4977a3b7468b73885
marcus-bornman <marcus.bornman@frei.one>
2020-06-07 20:53:52 +0200

    Attempted fix for broken core workflow

--- .github/scripts/decrypt_service_account_key.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Decrypt the file
-mkdir $HOME/secrets
-gpg --quiet --batch --yes --decrypt --passphrase="$GCP_CREDENTIALS_PASSWORD" \
---output $GITHUB_WORKSPACE/service-account-key.json .github/encrypted/service-account-key.json.gpg
