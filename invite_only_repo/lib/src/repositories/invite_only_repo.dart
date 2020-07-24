4cda680883fb4f066616b8fe33b1649ebd3193f2
marcus-bornman <marcus.bornman@frei.one>
2020-07-24 07:39:38 +0200

    Connect to localhost when running in debug mode

--- invite_only_repo/lib/src/repositories/invite_only_repo.dart
+++ invite_only_repo/lib/src/repositories/invite_only_repo.dart
@@ -17,7 +17,9 @@ abstract class InviteOnlyRepo {
   ///
   /// If [initialize] has not been called, this will be null.
   static InviteOnlyRepo get instance =>
-      InviteOnlyRepoImpl.getInstance('https://core.inviteonly.born.dev');
+      InviteOnlyRepoImpl.getInstance(kReleaseMode
+          ? 'https://core.inviteonly.born.dev'
+          : 'http://localhost:8080');
 
   /// Returns the current user's phone number, or null if there is no authenticated user.
   Future<String> currentUser();
