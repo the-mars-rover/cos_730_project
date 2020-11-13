a64ea0635152f67a369dfa6c42dbb256bc476d5d
marcus-bornman <marcus.bornman@frei.one>
2020-11-13 10:54:26 +0200

    Removed unnecessary files

--- invite_only_core/scripts/decrypt_service_account_key.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# This script is used to decrypt the service-account-key for the Google cloud platform service account.
-gpg --quiet --batch --yes --decrypt --passphrase="$GCP_CREDENTIALS_PASSWORD" \
---output service-account-key.json scripts/service-account-key.json.gpg
